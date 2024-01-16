b588386917f658382c8affd7e19e156ecd81d384
RiccardoPollice <riccardo.pollice17@gmail.com>
2024-01-16 14:59:13 +0100

    Initial commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO pizze(name, description, url, price ) VALUES ('Pizza margherita', 'Pizza sublime con pomodoro e mozzarella', 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Pizza_Margherita_stu_spivack.jpg/1280px-Pizza_Margherita_stu_spivack.jpg',4.00);
+INSERT INTO pizze(name, description, url, price ) VALUES ('Pizza diavola', 'Pizza preparata con pomodoro, mozzarella, salame calabrese', 'https://www.misya.info/wp-content/uploads/2007/08/Pizza-alla-diavola.jpg', 6.00);
